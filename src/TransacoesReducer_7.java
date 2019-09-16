71fa57e5107b01c2d98427214baea5f856a69184
Thiago Jose Fagundes Saquette <43481302+thigosaquette@users.noreply.github.com>
2019-09-16 19:42:55 -0300

    Feito, não testado.

--- src/TransacoesReducer_7.java
+++ src/TransacoesReducer_7.java
@@ -1,4 +1,31 @@
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
-public class TransacoesReducer_7 extends Reducer {
+import java.io.IOException;
+
+public class TransacoesReducer_7 extends Reducer<Text, TransacoesWritable, Text, FloatWritable> {
+
+    public void reduce(Text key,
+                       Iterable<TransacoesWritable> values,
+                       Context context) throws IOException, InterruptedException {
+
+        float valorPeso = 0;
+
+        // Para cada valor
+        for (TransacoesWritable v : values
+        ) {
+            float temp = (v.getValor() / v.getPeso());
+            if (valorPeso < temp) {
+                valorPeso = temp;
+            }
+        }
+
+        // Variável de saída
+        float Maior_valor_peso = valorPeso;
+        FloatWritable saida = new FloatWritable(Maior_valor_peso);
+
+        context.write(key, saida);
+
+    }
 }
