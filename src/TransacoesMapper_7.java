c66641797b85fe6f00cce6e98d77b09def63b041
Thiago Jose Fagundes Saquette <43481302+thigosaquette@users.noreply.github.com>
2019-09-16 19:42:03 -0300

    Feito, não testado

--- src/TransacoesMapper_7.java
+++ src/TransacoesMapper_7.java
@@ -1,4 +1,35 @@
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-public class TransacoesMapper_7 extends Mapper {
+public class TransacoesMapper_7 extends Mapper<LongWritable, Text, Text, TransacoesWritable>{
+
+    public void map(LongWritable key, Text value, Context context) throws IOException,
+            InterruptedException {
+
+        // Cada linha
+        String conteudo = value.toString();
+        // Separar por colunas
+        String[] valores = conteudo.split(";");
+
+        // Ignora o header e vazio
+        if (valores[5].equals("")) return;
+        if (valores[6].equals("weight_kg") || valores[6].equals("")) return;
+
+        // A chave é a mercadoria
+        Text chave = new Text(valores[3]);
+
+        // O valor é o valor / peso
+        TransacoesWritable valor = new TransacoesWritable();
+        valor.setN(1);
+        valor.setValor(Long.parseLong(valores[5]));
+        valor.setPeso(Long.parseLong(valores[6]));
+
+
+        // Passando isso pro reduce
+        context.write(chave, valor);
+
+
+    }
 }
